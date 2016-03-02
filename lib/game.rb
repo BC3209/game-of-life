92a5881f05e7657f6efa755bfa464d55632a8c26
BC3209 <bchidoro9@gmail.com>
2016-03-02 14:09:51 -0500

    Implemented Rule:1

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,4 @@
- # Game Class to interact with World and Cell Class
+# Game Class to interact with World and Cell Class
 class Game
   attr_accessor :world, :cells
 
@@ -11,33 +11,36 @@ class Game
     end
   end
 
-  # def evolve?
-  # Rule: 1
-  #   board.cells.each do |cell|
-  #     if cell.alive? && cell.live_neighbor_count(cell).count < 2
-  #       cell.die!
-  #     end
-  #   end
+    def evolve?
+      # Rule: 1
+      world.cells.each do |cell|
+        if cell.alive && world.live_neighbor_count(cell).count < 2
+          cell.die!
+      end
+    end
   end
+end
 
 
 # WORLD CLASS
 class World
-  attr_accessor :rows, :columns, :board
+  attr_accessor :rows, :columns, :board, :cells
 
   def initialize(rows=3, columns=3)
     @rows = rows
     @columns = columns
-
+    @cells = []
     @board = Array.new(rows) do |rows|
-               Array.new(columns) do |columns|
-                 Cell.new(rows, columns)
-                 #   0,0   0,1    0,2
-                 # [[Cell, Cell, Cell],
-                 #   1,0   1,1    1,2
-                 #  [Cell, Cell, Cell],
-                 #   2,0   2,1    2,2
-                 #  [Cell, Cell, Cell]]
+      Array.new(columns) do |columns|
+        cell = Cell.new(rows, columns)
+        cells << cell
+        cell
+        #   0,0   0,1    0,2
+        # [[Cell, Cell, Cell],
+        #   1,0   1,1    1,2
+        #  [Cell, Cell, Cell],
+        #   2,0   2,1    2,2
+        #  [Cell, Cell, Cell]]
       end
     end
   end
@@ -50,12 +53,27 @@ class World
       live_neighbors << live_cell if live_cell.alive?
     end
     # Neighbors to the NorthEast
+    live_cell = board[cell.y - 1][cell.x + 1]
+    live_neighbors << live_cell if live_cell.alive?
     # Neighbors to the SouthEast
+    live_cell = board[cell.y + 1][cell.x + 1]
+    live_neighbors << live_cell if live_cell.alive?
     # Neighbors to the SouthWest
+    live_cell = board[cell.y + 1][cell.x - 1]
+    live_neighbors << live_cell if live_cell.alive?
     # Neighbors to the NorthWest
+    live_cell = board[cell.y - 1][cell.x - 1]
+    live_neighbors << live_cell if live_cell.alive?
     # Neighbors to the East
+    live_cell = board[cell.y][cell.x + 1]
+    live_neighbors << live_cell if live_cell.alive?
     # Neighbors to the South
+    live_cell = board[cell.y + 1][cell.x]
+    live_neighbors << live_cell if live_cell.alive?
     # Neighbors to the West
+    live_cell = board[cell.y][cell.x - 1]
+    live_neighbors << live_cell if live_cell.alive?
+
     live_neighbors
   end
 end
